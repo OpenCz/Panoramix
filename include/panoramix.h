5a7526840c05fa5189dd8108104c318299bfbc01
Celz-Pch <celenzo.peuch@epitech.eu>
2026-04-28 20:01:06 +0200

    feat: include file with parse proto

---
+++ include/panoramix.h
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2026
+** panoramix
+** File description:
+** panoramix
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef PANO_H
+    #define PANO_H
+
+typedef struct panoramix_s {
+    int nb_villagers;
+    int pot_size;
+    int nb_fights;
+    int nb_refills;
+} panoramix_t;
+
+typedef struct parse_s {
+    int nb_arg;
+    char *name;
+} parse_t;
+
+panoramix_t *parse_arg(char **argv, panoramix_t *stock);
+void print_usage(void);
+#endif
